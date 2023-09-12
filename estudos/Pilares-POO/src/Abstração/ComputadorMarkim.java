f0812100d596cecfd2c2fa15097f16f2df7d695f
Josemarques Freitas <seujosemar@hotmail.com>
2023-09-12 11:25:45 -0300

    Conceitos-de-Abstração

---
+++ estudos/Pilares-POO/src/Abstração/ComputadorMarkim.java
@@ -0,0 +1,25 @@
+package Abstração;
+
+public class ComputadorMarkim {
+    public static void main(String[] args) {
+        System.out.println("MSN");
+
+        MSNMessenger msn = new MSNMessenger();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+
+        System.out.println("Facebook");
+
+        FacebookMessenger fbm = new FacebookMessenger();
+        fbm.enviarMensagem();
+        fbm.receberMensagem();
+
+        System.out.println("Telegram");
+
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
+
+    }
+
+}
---
+++ estudos/Pilares-POO/src/Abstração/ServicoMensagemInstantanea.java
@@ -0,0 +1,7 @@
+package Abstração;
+
+public abstract class ServicoMensagemInstantanea {
+    public abstract void enviarMensagem();
+
+    public abstract void receberMensagem();
+}
