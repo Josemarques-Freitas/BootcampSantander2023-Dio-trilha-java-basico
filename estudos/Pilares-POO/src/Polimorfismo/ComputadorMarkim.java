73f5de8f6d77b6fb0236ff4236da8c8d099e0cb1
Josemarques Freitas <seujosemar@hotmail.com>
2023-09-12 12:23:30 -0300

    Conceito-de-Polimorfismo

---
+++ estudos/Pilares-POO/src/Polimorfismo/ComputadorMarkim.java
@@ -0,0 +1,29 @@
+package Polimorfismo;
+
+import Polimorfismo.Apps.FacebookMessenger;
+import Polimorfismo.Apps.MSNMessenger;
+import Polimorfismo.Apps.ServicoMensagemInstantanea;
+import Polimorfismo.Apps.Telegram;
+
+public class ComputadorMarkim {
+    public static void main(String[] args) {
+
+        ServicoMensagemInstantanea smi = null;
+
+        /*
+         * NÃO SE SABE QUAL APP
+         * MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+         */
+        String appEscolhido = "msn";
+
+        if (appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
+        else if (appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+        else if (appEscolhido.equals("tlg"))
+            smi = new Telegram();
+
+        smi.enviarMensagem();
+        smi.receberMensagem();
+    }
+}
