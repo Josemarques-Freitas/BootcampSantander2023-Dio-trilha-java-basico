8a96c75b1a14d17e54f49960c2337af26795aabe
Josemarques Freitas <seujosemar@hotmail.com>
2023-09-14 17:14:06 -0300

    Atividade-carrinho-de-compras

---
+++ Collections-java-api-2023/src/Main/java/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,67 @@
+package Main.java.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Itens> itensList;
+
+    public CarrinhoDeCompras() {
+        this.itensList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String Nome, double Preco, int quantidade) {
+        Itens itens = new Itens(Nome, Preco, quantidade);
+        this.itensList.add(itens);
+    }
+
+    public void removerItem(String nome) {
+        List<Itens> itensParaRemover = new ArrayList<>();
+        for (Itens i : itensList) {
+            if (i.getNome().equalsIgnoreCase(nome)) {
+                itensParaRemover.add(i);
+            }
+        }
+
+        itensList.removeAll(itensParaRemover);
+
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d;
+        for (Itens itens : itensList) {
+            double valorItem = itens.getPreco() * itens.getQuantidade();
+            valorTotal += valorItem;
+        }
+
+        return valorTotal;
+
+    }
+
+    public void exibirItens() {
+        System.out.println(this.itensList);
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras [itensList=" + itensList + "]";
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("Lápis", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Lápis", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Caderno", 35d, 1);
+        carrinhoDeCompras.adicionarItem("Borracha", 2d, 2);
+
+        carrinhoDeCompras.exibirItens();
+
+        carrinhoDeCompras.removerItem("Lápis");
+
+        carrinhoDeCompras.exibirItens();
+
+        System.out.println("O valor total da compra é: " + carrinhoDeCompras.calcularValorTotal());
+    }
+}
---
+++ Collections-java-api-2023/src/Main/java/OperacoesBasicas/Itens.java
@@ -0,0 +1,31 @@
+package Main.java.OperacoesBasicas;
+
+public class Itens {
+    private String Nome;
+    private double Preco;
+    private int Quantidade;
+
+    public Itens(String nome, double preco, int quantidade) {
+        this.Nome = nome;
+        this.Preco = preco;
+        this.Quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public double getPreco() {
+        return Preco;
+    }
+
+    public int getQuantidade() {
+        return Quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Itens [Nome=" + Nome + ", Preco=" + Preco + ", Quantidade=" + Quantidade + "]";
+    }
+
+}
