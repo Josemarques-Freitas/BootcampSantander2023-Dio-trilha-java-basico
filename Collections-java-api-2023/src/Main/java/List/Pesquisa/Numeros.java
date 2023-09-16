e3377c8cd337ffd182bb0d2055f18545ddc9068c
Josemarques Freitas <seujosemar@hotmail.com>
2023-09-16 18:11:51 -0300

    Adicionando-atividade-List-Pesquisa

---
+++ Collections-java-api-2023/src/Main/java/List/Pesquisa/Numeros.java
@@ -0,0 +1,20 @@
+package Main.java.List.Pesquisa;
+
+public class Numeros {
+
+    private Integer numeros;
+
+    public Numeros(Integer numeros) {
+        this.numeros = numeros;
+    }
+
+    public Integer getNumeros() {
+        return numeros;
+    }
+
+    @Override
+    public String toString() {
+        return "Numeros = " + numeros;
+    }
+
+}
--- Collections-java-api-2023/src/Main/java/Map/Ordenação/AgendaEventos.java
+++ Collections-java-api-2023/src/Main/java/Map/Ordenação/AgendaEventos.java
@@ -49,4 +49,5 @@ public class AgendaEventos {
 
         agendaEventos.obterProximoEvento();
     }
+
 }
