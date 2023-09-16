9a2dbd99eba41f4639df5d48f01e0cde5dbdce12
Josemarques Freitas <seujosemar@hotmail.com>
2023-09-16 13:06:11 -0300

    Ordenação-Map

---
+++ Collections-java-api-2023/src/Main/java/Map/Ordenação/AgendaEventos.java
@@ -0,0 +1,52 @@
+package Main.java.Map.Ordenação;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class AgendaEventos {
+    private Map<LocalDate, Eventos> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        Eventos eventos = new Eventos(nome, atracao);
+        eventosMap.put(data, eventos);
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Eventos> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Eventos> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Eventos> entry : eventosTreeMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                System.out.println("O próximo evento: " + entry.getValue() + " acontecerá na data " + entry.getKey());
+                break;
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2020, Month.JANUARY, 10), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.SEPTEMBER, 15), "Evento 4", "Atração 4");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.SEPTEMBER, 25), "Evento 5", "Atração 5");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+}
