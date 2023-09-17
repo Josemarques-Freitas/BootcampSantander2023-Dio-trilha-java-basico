f6af5763f76bce323b7961edff0dd341ba584b30
Josemarques Freitas <seujosemar@hotmail.com>
2023-09-16 21:50:49 -0300

    Complementando-Atividade-01-de-Pesquisa-Map

--- Collections-java-api-2023/src/Main/java/Map/Pesquisa/EstoqueProdutos.java
+++ Collections-java-api-2023/src/Main/java/Map/Pesquisa/EstoqueProdutos.java
@@ -31,30 +31,63 @@ public class EstoqueProdutos {
     public Produto obterProdutoMaisCaro() {
         Produto produtoMaisCaro = null;
         double maiorPreco = Double.MIN_VALUE;
-        if (!estoqueProdutosMap.isEmpty()) {
-            for (Produto p : estoqueProdutosMap.values()) {
-                if (p.getPreco() > maiorPreco) {
-                    produtoMaisCaro = p;
-                }
+        for (Produto p : estoqueProdutosMap.values()) {
+            if (p.getPreco() > maiorPreco) {
+                produtoMaisCaro = p;
+                maiorPreco = p.getPreco();
             }
         }
         return produtoMaisCaro;
     }
 
+    public Produto obterProdutoMaisBarato() {
+        Produto produtoMaisBarato = null;
+        double menorPreco = Double.MAX_VALUE;
+        for (Produto p : estoqueProdutosMap.values()) {
+            if (p.getPreco() < menorPreco) {
+                produtoMaisBarato = p;
+                menorPreco = p.getPreco();
+            }
+        }
+        return produtoMaisBarato;
+    }
+
+    public Produto obterProdutoMaiorQuantidadeValorTotalNoEstoque() {
+        Produto produtoMaiorQuantidadeValorNoEstoque = null;
+        double maiorValorTotalProdutoEstoque = 0d;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Map.Entry<Long, Produto> entry : estoqueProdutosMap.entrySet()) {
+                double valorProdutoEmEstoque = entry.getValue().getPreco() * entry.getValue().getQuantidade();
+                if (valorProdutoEmEstoque > maiorValorTotalProdutoEstoque) {
+                    maiorValorTotalProdutoEstoque = valorProdutoEmEstoque;
+                    produtoMaiorQuantidadeValorNoEstoque = entry.getValue();
+                }
+            }
+        }
+        return produtoMaiorQuantidadeValorNoEstoque;
+    }
+
     public static void main(String[] args) {
         EstoqueProdutos estoque = new EstoqueProdutos();
-        estoque.exibirProdutos();
-
-        estoque.adicionarProduto(1L, " Produto A", 10, 5.0);
-        estoque.adicionarProduto(2L, " Produto B", 5, 10.0);
-        estoque.adicionarProduto(3L, " Produto C", 2, 15.0);
-        estoque.adicionarProduto(8L, " Produto D", 2, 20.0);
 
         estoque.exibirProdutos();
 
-        System.out.println("Valor total do estoque: R$" +
-                estoque.calcularValorTotalEstoque());
-        System.out.println("Produto mais caro é o " + estoque.obterProdutoMaisCaro());
+        estoque.adicionarProduto(1L, " Notebook", 1, 1500.0);
+        estoque.adicionarProduto(2L, " Mouse", 5, 25.0);
+        estoque.adicionarProduto(3L, " Monitor", 10, 400.0);
+        estoque.adicionarProduto(8L, " Teclado", 2, 40.0);
 
+        estoque.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+
+        Produto produtoMaisCaro = estoque.obterProdutoMaisCaro();
+        System.out.println("Produto mais caro é o " + produtoMaisCaro);
+
+        Produto produtoMaisBarato = estoque.obterProdutoMaisBarato();
+        System.out.println("Produto mais barato: " + produtoMaisBarato);
+
+        Produto produtoMaisQuantidadeValorTotal = estoque.obterProdutoMaiorQuantidadeValorTotalNoEstoque();
+        System.out.println("Produto com maior quantidade em valor no estoque: " + produtoMaisQuantidadeValorTotal);
     }
 }
