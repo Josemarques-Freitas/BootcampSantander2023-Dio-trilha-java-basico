606a3420d95f73ba8bb978868442da77f8305a1c
Josemarques Freitas <seujosemar@hotmail.com>
2023-09-18 08:40:52 -0300

    Exemplo-de-Consumer

---
+++ Ganhando_produtividade_com_Stream_API_Java/src/Functional_Interface/Exemplos/ConsumerExample.java
@@ -0,0 +1,20 @@
+package Functional_Interface.Exemplos;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.print(numero + " ");
+            }
+        };
+
+        numeros.stream().forEach(imprimirNumeroPar);
+    }
+}
